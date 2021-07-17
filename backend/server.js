bbe4f61017d514fe0adce0352878abc326aaf71a
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2021-07-17 11:29:58 +0200

    before mentora

--- backend/server.js
+++ backend/server.js
@@ -1,62 +1,76 @@
 const express = require("express");
-const cors = require ('cors');
+const cors = require("cors");
 const mysql = require("mysql2");
 const app = express();
 
-const db = mysql.createPool({
-    host: "localhost",
+const db = mysql.createConnection({
+    host: 'localhost',
     user: 'root',
     password: 'pz3YIiTtX1%:K5J',
     database: 'groupomania',
 });
 
 app.use(cors());
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/get', (req, res) => {
-    const sqlSelect = 
-        'SELECT * FROM articles';
-    db.query(sqlSelect, (err, result) => {
-        res.send(result);
-});
-})
-
-app.post('/api/insert', (req,res) => {
-
-    const articleTitle = req.body.articleTitle
-    const articleContent = req.body.articleContent
-    const articleAuthor = req.body.articleAuthor
-
-    const sqlInsert = 
-        'INSERT INTO articles (title, content, author) VALUES (?,?,?)';
-    db.query(sqlInsert, [articleTitle, articleContent, articleAuthor], (err, result) => {
-        console.log(result);
-    });
+app.get("/api/get", (req, res) => {
+  const sqlSelect = "SELECT * FROM articles";
+  db.query(sqlSelect, (err, result) => {
+      if (err) {
+            console.log(err)
+      } else {
+            res.send(result);
+      }
+  });
 });
 
-app.delete('/api/delete/:id', (req,res) => {
-    const articleId = req.params.id
-    const sqlDelete = 
-        "DELETE FROM articles WHERE id = ?";
+app.post("/api/insert", (req, res) => {
+  const articleTitle = req.body.articleTitle;
+  const articleContent = req.body.articleContent;
+  const articleAuthor = req.body.articleAuthor;
 
-    db.query(sqlDelete, articleId, (err, result) => {
-       if (err) console.log(err);
-    });
+  const sqlInsert =
+    "INSERT INTO articles (title, content, author) VALUES (?,?,?)";
+  db.query(sqlInsert,[articleTitle, articleContent, articleAuthor],(err, result) => {
+      if (err) {
+          console.log(err)
+      } else {
+          res.send('Article enregistré !')
+      }
+    }
+  );
 });
 
-app.put('/api/update', (req,res) => {
-    const articleId = req.params.articleId
-    const articleContent = req.body.articleContent
-    const sqlUpdate = 
-        "UPDATE articles SET content = ? WHERE id = ?";
+app.delete("/api/delete/:id", (req, res) => {
+  const articleId = req.params.id;
+  const sqlDelete = "DELETE FROM articles WHERE id = ?";
 
-    db.query(sqlUpdate, [articleContent, articleId], (err, result) => {
-       if (err) console.log(err);
-       console.log(result);
-    });
+  db.query(sqlDelete, articleId, (err, result) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("Article supprimé !")
+    };
+  });
+});
+
+app.put("/api/update/:id", (req, res) => {
+  console.log("PARAMS", req.params, "BODY : ", req.body) //debug
+  const articleId = req.params.articleId;
+  const articleTitle = req.body.newArticleTitle;
+  const articleContent = req.body.newArticleContent;
+  const sqlUpdate = "UPDATE articles SET title = ?, content = ? WHERE id = ?";
+
+  db.query(sqlUpdate, [articleTitle, articleContent, articleId], (err, result) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.send(result)
+      console.log("Article modifié !", result)}
+  });
 });
 
 app.listen(3001, () => {
-    console.log("running on port 3001");
+  console.log("running on port 3001");
 });
--- groupomania/src/App.js
+++ groupomania/src/App.js
@@ -1,101 +1,103 @@
-import React, { useState, useEffect } from "react";
-import './App.css';
-import Axios from "axios"
+import { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [articleTitle, setArticleTitle] = useState("");
+  const [articleContent, setArticleContent] = useState("");
+  const [articleAuthor, setArticleAuthor] = useState("");
 
-  const [articleTitle, setArticleTitle] = useState('')
-  const [articleContent, setArticleContent] = useState('')
-  const [articleAuthor, setArticleAuthor] = useState('')
+  const [articleList, setArticleList] = useState([]);
 
-  const [articleList, setArticleList] = useState([])
+  const [newContent, setNewContent] = useState("");
+  const [newTitle, setNewTitle] = useState("");
 
-  const [newContent, setNewContent] = useState('')
-  const [newTitle, setNewTitle] = useState('')
-
-  useEffect( () => {
-    Axios.get('http://localhost:3001/api/get').then((response) =>{
-      setArticleList([response.data])
-    })
-  }, [])
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response) => {
+      setArticleList(response.data);
+    });
+  }, []);
 
   const submitArticle = () => {
-
-    Axios.post('http://localhost:3001/api/insert',{ 
-      articleTitle: articleTitle, 
+    Axios.post("http://localhost:3001/api/insert", {
+      articleTitle: articleTitle,
       articleContent: articleContent,
       articleAuthor: articleAuthor,
-    });
-
-    setArticleList([
-      ...articleList, 
-      {articleTitle: articleTitle, articleContent: articleContent, articleAuthor: articleAuthor}
-    ]);
+    }).then(() => {
+      setArticleList([
+        ...articleList,
+        {
+          articleTitle: articleTitle,
+          articleContent: articleContent,
+          articleAuthor: articleAuthor,
+        },
+      ]);
+    })
   };
 
   const deleteArticle = (id) => {
-
     Axios.delete(`http://localhost:3001/api/delete/${id}`);
     //ajouter la mise à jour de la liste d'article
   };
 
   const updateArticle = (id) => {
-    Axios.put('http://localhost:3001/api/update', {
+    console.log(id, newTitle, newContent)
+    Axios.put(`http://localhost:3001/api/update/${id}`, {
       articleId: id,
-      articleTitle: newTitle,
-      articleContent: newContent,
+      newArticleTitle: newTitle,
+      newArticleContent: newContent,
     });
 
     //ajouter la mise à jour de l'article automatique
-    setArticleContent('')
-  }
+    setNewContent("");
+    setNewTitle("");
+  };
 
   return (
     <div className="App">
       <h1>Réseau social d'entreprise - Groupomania </h1>
-      
-      <div className='form'>
+
+      <div className="form">
+        <h2>Poster un article : </h2>
         <label> Titre de l'article</label>
-        <input type="text" name='articleTitle' onChange={(e) => {
-          setArticleTitle(e.target.value)
-        }}
-        />
+        <input type="text" name="articleTitle" onChange={(e) => {setArticleTitle(e.target.value);}}/>
         <label> Contenu de l'article </label>
-        <input type="text" name='content' onChange={(e) => {
-          setArticleContent(e.target.value)
-        }}
-        />
+        <textarea type="text" name="content" onChange={(e) => {setArticleContent(e.target.value);}}/>
         <label> Auteur de l'article </label>
-        <input type="text" name='author' onChange={(e) => {
-          setArticleAuthor(e.target.value)
-        }}
-        />
+        <input type="text" name="author" onChange={(e) => {setArticleAuthor(e.target.value);}}/>
 
-        <button onClick={submitArticle}>Submit</button>
+        <button className="btn" onClick={submitArticle}>
+          Submit
+        </button>
+      </div>
+      <div className="article-list">
+        {articleList.map((article, index) => {
+          return (
+            <div key={`${article}-${index}`} className="card">
+              <div className="article-display">
+                <h2 className="article-display-title"> {article.title} </h2>
+                <p> {article.content} </p>
+                <h3 className="article-display-author">Publié par {article.author} </h3>
+              </div>
+              
+              <div className="article-modification">
+                <span className='border'></span>
+                <h3> Modifier l'article : </h3>
+                <div className='article-modification-title'>
+                  <label> Nouveau titre :</label>
+                  <input className="newTitle updateInput" type="text" placeholder="Nouveau titre" onChange={(e) => {setNewTitle(e.target.value);}}/>
+                </div>
+                <label> Nouveau corps de l'article </label>
+                <textarea className="newContent updateInput" type="text" placeholder="Nouveau contenu" onChange={(e) => {setNewContent(e.target.value);}}/>
+                <button className="btn" onClick={() => {updateArticle(article.id);}}>Modifier l'article</button>
+                <span className='border'></span>
+                <button className="btn" onClick={() => {deleteArticle(article.id);}}>Supprimer l'article</button>
+              </div>
+                
 
-        {articleList.map((val) => {
-          if (val.articleTitle)
-            return (
-            <div className='card'>
-              <h2>{val.articleTitle} </h2> 
-              <p>{val.articleContent}</p>
-              <h3>{val.articleAuthor}</h3>
-              <p>Article publié le {val.date_publication}</p>
-
-              <button onClick={ () => {deleteArticle (val.id)}}> Delete </button>
-              <label> Modifier le titre de l'article : </label>
-              <input type="text" id='updateInput' onChange={ (e) => {
-                setNewTitle(e.target.value)
-              }}
-              />
-              <label> Modifier le contenu de l'article : </label>
-              <input type="text" id='updateInput' onChange={ (e) => {
-                setNewContent(e.target.value)
-              }}
-              />
-              <button onClick={() => {updateArticle(val.id)}}> Update </button>
+            
             </div>
-            )
+          );
         })}
       </div>
     </div>
