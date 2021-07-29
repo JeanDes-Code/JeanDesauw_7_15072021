aa68b28c1781898409ec0dd9151f4e883e7b4c80
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2021-07-29 15:41:13 +0200

    correction d'un bug modification commentaire

--- groupomania/src/services/put-request.js
+++ groupomania/src/services/put-request.js
@@ -11,7 +11,7 @@ const putRequest = (id, data, articleId) => {
       },
     });
   } else {
-    Axios.put(`http://localhost:3001/api/commentaires/put/${articleId}/${id}`, data , 
+    Axios.put(`http://localhost:3001/api/commentaires/put/${articleId}/${id}`, {data} , 
     {
       headers: {
         Authorization: `Bearer ${token}`,
